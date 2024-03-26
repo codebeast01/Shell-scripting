f4f18a2874a95b2ab17f26313401a55e8f95d4c1
codebeast01 <maahm061@gmail.com>
2024-03-26 15:10:18 +0300

    First commit

---
+++ hello.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# This is a comment 
+# Not excecutable
+# "Hello Nima"
+
+#Variable (System)
+#echo $BASH
+#echo $HOME
+
+#echo $PWD
+
+#name=Abdallah
+#echo The name is $name
+
+echo "Enter name : "
+read name 
+echo "Entered Name : $name"
